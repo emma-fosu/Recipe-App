50b384d79b74907ec7e378ac005ae4137e666837
Emmanuel Fosu <emmanuel.fosuduffour@gmail.com>
2025-01-09 10:36:09 +0000

    Added new-recipe component to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NewRecipeComponent } from './new-recipe/new-recipe.component';
 
 export const routes: Routes = [
     {
         path: "recipes",
         component: RecipesListComponent,
         title: "My Recipes",
+    },
+    {
+        path: "new-recipes",
+        component: NewRecipeComponent,
+        title: "Add New Recipe"
     },
     {
         path: "",
